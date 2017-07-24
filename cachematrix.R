f4b191adf4a74726299a09ebc8115a239f8b56d6
jsmall6 <james.small@outlook.com>
2017-07-24 17:27:22 -0400

    Assignment 2 check in
    Checking in changes for assignment 2. Complete functions with comments.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #null out the object variable m
+  m <- NULL
+  
+  #setter function
+  #sets x of the parent object to be the matrix passed to argument y
+  #null out the object variable m
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #Getter - Return x
+  get <- function() x
+  
+  #Setter setMatrix set the function solve to the m object of the parent
+  setMatrix <- function(solve) m <<- solve
+  
+  #getter - return the object m
+  getMatrix <- function() m
+  
+  #setting attributes of the functions for easiers innvocation
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
 
 
@@ -12,4 +35,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  #call getter function of the object 'x' passed in 
+  m <- x$getMatrix()
+  
+  #If m is null it means solve has not been invoked on the matrix yet.
+  #Else m was solved previous and is in the cache of object 'x' return the previously solved inverse of 'x'
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  #Else m is null; set data by getting 'x' and call solve on data to store in m local.
+  data <- x$get()
+  m <- solve(data, ...)
+  #call the setter to set the solve of 'x' and effectively cache the result
+  x$setMatrix(m)
+  #return the solve of the matrix. Alternatively the x$getMatrix() could have been called.
+  m
 }
